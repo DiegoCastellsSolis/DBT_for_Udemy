1c01f0412c07715b7c210b5238ab1c27ebc08034
Data-Grove-BI <155761069+Data-Grove-BI@users.noreply.github.com>
2024-10-31 11:32:00 +0100

    changes

--- models/nova_bi_dw/DIM_CATEGORIA.sql
+++
@@ -1,7 +0,0 @@
--- models/DIM_CATEGORIA.sql
-{{ config(materialized='table') }}  -- Asegúrate de que esta línea esté en la parte superior
-
-SELECT DISTINCT
-    ROW_NUMBER() OVER (ORDER BY CATEGORIA) AS CATEGORIA_ID, -- Generar un ID único
-    CATEGORIA
-FROM {{ source('STAGING', 'experiencias') }} 
--- models/nova_bi_dw/DIM_UBICACION.sql
+++
@@ -1,7 +0,0 @@
--- models/DIM_UBICACION.sql
-{{ config(materialized='table') }}  -- Asegúrate de que esta línea esté en la parte superior
-
-SELECT DISTINCT
-    ROW_NUMBER() OVER (ORDER BY UBICACION) AS UBICACION_ID, -- Generar un ID único
-    UBICACION
-FROM {{ source('STAGING', 'experiencias') }} 
